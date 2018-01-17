91f5187b6b760ebb20a2ec5dd3f7073b9ba485c5
Adjoa <adjoa.darien@gmail.com>
2018-01-17 17:49:21 -0500

    initial CLI interface

--- lib/hack_design/cli.rb
+++ lib/hack_design/cli.rb
@@ -1,7 +1,42 @@
 module HackDesign
     class CLI
         def start
-            puts "Hello, world!"
+            puts "=== Hack Design CLI ==="
+            main_menu
+        end
+
+        def main_menu
+            puts "Enter a number to select an option"
+            puts " 1- List categories" 
+            puts " 2- List all lessons" 
+            puts " 3- Display a random lesson"
+            puts "Enter 'q' to quit"
+            puts ""
+            selection = gets.strip
+            case(selection)
+            when '1'
+                puts "Categories"
+                puts "----------"
+                puts ""
+                main_menu
+            when '2'
+                puts "Lessons"
+                puts "----------"
+                puts ""
+                main_menu
+            when '3'
+                puts "Random Lesson"
+                puts "----------"
+                puts ""
+                main_menu
+            when 'q'
+                puts "Same time tomorrow, okay? Goodbye!"
+                exit
+            else
+                puts "You have not selected a valid option."
+                puts ""
+                main_menu
+            end
         end
     end
 end
