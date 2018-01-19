092865d2650be1903fbacd26a5ed3ac3c8863347
Adjoa <adjoa.darien@gmail.com>
2018-01-18 21:26:21 -0500

    added init Exercise class

---
+++ lib/hack_design/exercise.rb
@@ -0,0 +1,9 @@
+class Exercise
+    attr_accessor :url, :title, :description
+
+    def initialize(title)
+        @url = url
+        @title = title
+        @description = description
+    end
+end
