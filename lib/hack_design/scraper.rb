2e4847f6cc26c78eabbb12c5d08b913591d095bd
Adjoa Darien <adjoa.darien@gmail.com>
2018-01-23 22:17:21 +0000

    Scraper.scrape_index_page gets categories

--- lib/hack_design/scraper.rb
+++ lib/hack_design/scraper.rb
@@ -3,8 +3,16 @@ require 'open-uri'
 
 class Scraper
   def self.scrape_index_page(index_url)
+    categories = []
     doc = Nokogiri::HTML(open(index_url))
 
+    doc.css('div.overflow-hidden.mb4').each_with_index do |category, index|
+      name = category.css('h4').text.strip
+      id = index
 
+      categories << { name: name, id: id }
+    end
+    binding.pry
+    categories
   end
 end
