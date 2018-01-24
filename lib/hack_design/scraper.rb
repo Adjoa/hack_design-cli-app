22b97e6b38d47ae572e10fd4b5aaee29bb66cbbe
Adjoa Darien <adjoa.darien@gmail.com>
2018-01-24 00:52:17 +0000

    TODO: scrape lessons for each category

--- lib/hack_design/scraper.rb
+++ lib/hack_design/scraper.rb
@@ -5,14 +5,20 @@ class Scraper
   def self.scrape_index_page(index_url)
     categories = []
     doc = Nokogiri::HTML(open(index_url))
-
+    binding.pry
     doc.css('div.overflow-hidden.mb4').each_with_index do |category, index|
       name = category.css('h4').text.strip
       id = index
 
-      categories << { name: name, id: id }
+      lessons = []
+      # category.css('+div h3') #=> "Hello World"
+      # category.css('+div h3').each.with_index do |lesson, index|
+      #   lessons << { lesson_title:lesson.text, lesson_id: index }
+      # end
+
+      categories << { name: name, id: id, lessons: lessons }
     end
-    binding.pry
+
     categories
   end
 end
