33b915c614bb1e97be52f85463adb79458d7216c
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-03 22:41:47 +0000

    removed references to Category from scraper.rb

--- lib/hack_design/scraper.rb
+++ lib/hack_design/scraper.rb
@@ -3,22 +3,12 @@ require 'open-uri'
 
 class Scraper
   def self.scrape_index_page(index_url)
-    categories = []
     doc = Nokogiri::HTML(open(index_url))
     binding.pry
-    doc.css('div.overflow-hidden.mb4').each_with_index do |category, index|
-      name = category.css('h4').text.strip
-      id = index
 
-      lessons = []
-      # category.css('+div h3') #=> "Hello World"
-      # category.css('+div h3').each.with_index do |lesson, index|
-      #   lessons << { lesson_title:lesson.text, lesson_id: index }
-      # end
-
-      categories << { name: name, id: id, lessons: lessons }
+    lessons = []
+    doc.css('div.flex-center.mb4').each_with_index do |lesson, index|
+        lessons << {id: index,  title:lesson.text}
     end
-
-    categories
   end
 end
