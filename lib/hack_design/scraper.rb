8d7d4caab149408750fd80ae8b76a18bc8802f9b
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-05 18:12:38 +0000

    composed nokogiri queries for lesson instructor, intro, and exercises

--- lib/hack_design/scraper.rb
+++ lib/hack_design/scraper.rb
@@ -14,9 +14,22 @@ class Scraper
       title = lesson.css('h3').text
       self.lessons << {id: index,  title: title}
     end
-    binding.pry
   end
 
   def self.scrape_lesson
+    base_url = './fixtures/site/lessons/'
+    self.lessons.each_with_index do |lesson, index|
+      lesson_url = base_url + lesson[:id].to_s
+      doc = Nokogiri::HTML(open(lesson_url))
+      #instructor = doc.css('div.mr2 h2').text.strip
+      #introduction = doc.css('div.mr2 h2').text.strip
+      #exercises =
+      # doc.css('div.mb4 ol').each do |exercise|
+      #  url = exercise.css('a').attribute('href').text
+      #  title = exercise.css('li a').text.strip
+      #  description = exercise.css('li p').text.strip
+      #  puts ""
+      # end
+    end
   end
 end
