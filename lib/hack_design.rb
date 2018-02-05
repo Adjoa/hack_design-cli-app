55783cac4cabd6993a1885110ab99cb1ed082835
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-05 20:49:12 +0000

    removed exercise.rb

--- lib/hack_design.rb
+++ lib/hack_design.rb
@@ -1,5 +1,4 @@
 require_relative "./hack_design/scraper"
-require_relative "./hack_design/exercise"
 require_relative "./hack_design/lesson"
 require_relative "./hack_design/version"
 require_relative "./hack_design/cli"
--- lib/hack_design/exercise.rb
+++
@@ -1,9 +0,0 @@
-class Exercise
-    attr_accessor :url, :title, :description, :lesson
-
-    def initialize(title)
-        @url = url
-        @title = title
-        @description = description
-    end
-end
