9d08a18dc1eca8525fb5216f813ede6a750abc27
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-05 16:16:58 +0000

    added self.lessons to Scraper

--- lib/hack_design/scraper.rb
+++ lib/hack_design/scraper.rb
@@ -4,11 +4,15 @@ require 'open-uri'
 class Scraper
   @@lessons = []
 
+  def self.lessons
+    @@lessons
+  end
+
   def self.scrape_lessons_page
     doc = Nokogiri::HTML(open('./fixtures/site/lessons.html'))
     doc.css('div.prose').each_with_index do |lesson, index|
       title = lesson.css('h3').text
-      lessons << {id: index,  title: title}
+      self.lessons << {id: index,  title: title}
     end
     binding.pry
   end
