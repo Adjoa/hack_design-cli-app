0fbcb97c39f57c52052986b6774b356112cccb9f
Adjoa <adjoa.darien@gmail.com>
2018-10-12 22:57:28 +0000

    in scraper_spec, expect .scrape_lessons_page to return an Array

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,9 +1,12 @@
-require ' scraper'
-
-describe Scraper do
-  describe ".scrape_lessons_page" do
+RSpec.describe Scraper do
+  describe '.scrape_lessons_page' do
+    it 'returns an array of objects containing lesson title and url pairs' do
+      lessons_array = Scraper.scrape_lessons_page
+      
+      expect(lessons_array).to be_an(Array)
+    end
   end
   
-  describe ".scrape_lesson" do
-  end
+  # describe '.scrape_lesson' do
+  # end
 end
